863551108c58598c91942f0dd42cb4a0d15e6c94
vijaytadala2105 <96905511+vijaytadala2105@users.noreply.github.com>
2025-10-10 13:38:08 +0530

    Update app.py

--- ecs_stack/app.py
+++ ecs_stack/app.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 import os
 
-# import aws_cdk_lib as cdk
-from aws_cdk import App
+import aws_cdk as cdk
 
 from ecs_stack.ecs_stack_stack import EcsStackStack
 
 
-# app = cdk.App()
-app = App()
+app = cdk.App()
 EcsStackStack(app, "EcsStackStack",
           env=cdk.Environment(account='880729935461', region='us-east-1')    
      )
