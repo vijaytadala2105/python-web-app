90086805c949132ca17676be85582e31c581f98c
vijaytadala2105 <96905511+vijaytadala2105@users.noreply.github.com>
2025-10-10 09:22:54 +0530

    Update app.py

--- ecs_stack/app.py
+++ ecs_stack/app.py
@@ -8,7 +8,7 @@ from ecs_stack.ecs_stack_stack import EcsStackStack
 
 app = cdk.App()
 EcsStackStack(app, "EcsStackStack",
-          env=cdk.Environment(account='880729935461', region='us-east-1'),    
+          env=cdk.Environment(account='880729935461', region='us-east-1')    
      )
 
 app.synth()
